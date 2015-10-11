7dbded1f56bf0b95e25550100f2d45222144bb6c
AA <abdi.cg.ahmed@gmail.com>
2015-10-11 20:23:59 +0100

    added new route and view

--- app/routes.js
+++ app/routes.js
@@ -17,12 +17,15 @@ var routes = function(app, passport){
 
     //login page 
     app.get('/login', function(req, res){
+      if(req.isAuthenticated())
+        res.redirect('/dashboard');
+
       res.render('login', { message: req.flash('loginMessage') });
     });
 
     //process the login form 
-    app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/profile', // redirect to the secure profile section
+    app.post('/login',  passport.authenticate('local-login', {
+        successRedirect : '/dashboard', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
@@ -49,6 +52,12 @@ var routes = function(app, passport){
     app.get('/logout', function(req, res){
       req.logout();//this method provided by passport to logout the user
       res.redirect('/');
+    });
+
+    app.get('/dashboard', isLoggedIn, function(req, res){
+      res.render('dashboard', {
+        user: req.user
+      });
     });
 
 
