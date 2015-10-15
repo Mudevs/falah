194637cb3cce94e530968ec5bb0a6b275be3f138
AA <abdi.cg.ahmed@gmail.com>
2015-10-15 22:55:35 +0100

    added classes read from db and displayed on dashbaord

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,21 @@ $(document).ready(function(){
   
   });//end click function  
 
+
+
+  function renderClasses(element, data){
+
+    for(var i = 0; i<data.length; i++){
+      var element = document.createElement(element);
+      option.text = data[i].name;
+      option.value = data[i].name;
+      option.name = data[i].name;
+      console.log(data[i].name);
+
+      selectElement.appendChild(option);
+    }
+  }
+
   if(window.location.pathname === '/addstudent'){
     $.ajax('/getclasses')
   .done(function( data ) {
@@ -20,10 +35,24 @@ $(document).ready(function(){
 
       selectElement.appendChild(option);
     }
-    
-    
-      
+
   });      
+
+  } else if(window.location.pathname === '/dashboard'){
+    $.ajax('/getclasses')
+  .done(function( data ) {
+    var ulElement = document.getElementById('classes-list');
+    console.log(data);
+    for(var i = 0; i<data.length; i++){
+
+      var li = document.createElement('li');
+      li.innerHTML = data[i].name;
+      console.log(li);
+      ulElement.appendChild(li);
+
+    }
+     
+  });
 
   }
   
